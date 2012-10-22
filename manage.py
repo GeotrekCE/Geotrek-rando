d91eecdd0749eb62f0a98b54e61b0114d066254a
Gael Utard <gael.utard@makina-corpus.com>
2012-10-22 17:45:00 +0200

    Make manage.py work with virtualenv

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+activate_script = os.path.join(os.path.dirname(__file__), 'bin/activate_this.py')
+execfile(activate_script, {'__file__': activate_script})
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rando.settings")
 
