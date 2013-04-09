6eafd5a8e482e2e3014cc0aae75edc73b1356bb7
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-09 14:35:05 +0200

    Move jquery.smartresize to separate file, and fix invalidate maps

--- rando/static/js/application/ui.js
+++ rando/static/js/application/ui.js
@@ -1,32 +1,3 @@
-(function($,sr){
-
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-    var debounce = function (func, threshold, execAsap) {
-        var timeout;
-
-        return function debounced () {
-            var obj = this, args = arguments;
-            function delayed () {
-                if (!execAsap)
-                  func.apply(obj, args);
-                timeout = null;
-            };
-
-            if (timeout)
-                clearTimeout(timeout);
-            else if (execAsap)
-                func.apply(obj, args);
-
-            timeout = setTimeout(delayed, threshold || 100);
-        };
-    }
-  // smartresize 
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-})(jQuery,'smartresize');
-
-
 function init_ui() {
     $(document).pjax('a.pjax', '#content');
 
@@ -38,10 +9,9 @@ function init_ui() {
     window.backPack = new BackPack();
     $('body').on("backpack-change", refresh_backpack);
 
-    console.log('smart command not found');
-    // $(window).smart(function() {
-    //     invalidate_maps();
-    // });
+    $(window).smartresize(function() {
+         invalidate_maps();
+    });
 }
 
 function page_load() {
