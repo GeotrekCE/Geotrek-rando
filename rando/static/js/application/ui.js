7de0e7e060f5bcf588536f4b816ffa58a54d15ce
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-10 16:33:15 +0200

    Fix fullscreen and hide button if feature not detected

--- rando/static/js/application/ui.js
+++ rando/static/js/application/ui.js
@@ -1,3 +1,18 @@
+Modernizr.addTest('fullscreen', function(){
+     var ancelFullScreen = 'ancelFullScreen'; //make string minifiable
+
+     //FF9 pre-check
+     if(document.mozCancelFullScreen && !document.mozFullScreenEnabled) return false;
+
+     var limit = Modernizr._domPrefixes.length;
+     for(var i = 0; i < limit; ++i) {
+        if( document[[Modernizr._domPrefixes[i].toLowerCase(),'C',ancelFullScreen].join('')])
+            return true;
+     }
+     return !!document[['c',ancelFullScreen].join('')] || false;
+});
+
+
 function init_ui() {
     $(document).pjax('a.pjax', '#content');
 
