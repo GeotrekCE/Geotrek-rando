2c6a099cd93f27b27a1bfe6c4d71f779a6b02010
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-05-17 13:07:49 +0200

    Specific case for duration higher than 8 days (ref #471)

--- rando/trekking/templatetags/trekking.py
+++ rando/trekking/templatetags/trekking.py
@@ -31,13 +31,15 @@ def duration(value):
     seconds = timedelta(minutes=float(value) * 60)
     duration = datetime(1, 1, 1) + seconds
     days = duration.day - 1
-    print duration
+    if days >= 8:
+        return _("More than %s days") % 8
     if days > 1:
         return _("%s days") % days
-    if days > 0:
-        return _("%s day") % days
+    if days == 1 or 12 <= duration.hour < 24:
+        return _("%s day") % 1
     if duration.hour > 0:
-        return _("%(hour)sH%(min)s") % {'hour': duration.hour, 'min': "%s" % duration.minute if duration.minute > 0 else ""}
+        return _("%(hour)sH%(min)s") % {'hour': duration.hour,
+                                        'min': "%s" % duration.minute if duration.minute > 0 else ""}
     return _("%s min.") % duration.minute
 
 
