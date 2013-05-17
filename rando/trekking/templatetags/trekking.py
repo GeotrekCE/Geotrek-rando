1a1c9029795445af446659ce1726eb23d785ec7a
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-05-17 12:03:50 +0200

    Show duration as human readable

--- rando/trekking/templatetags/trekking.py
+++ rando/trekking/templatetags/trekking.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from datetime import datetime, timedelta
 
 from django import template
 from django.conf import settings
@@ -23,6 +24,21 @@ def fileinclude(filename):
 @register.filter
 def kilo(value):
     return value / 1000.0
+
+
+@register.filter
+def duration(value):
+    seconds = timedelta(minutes=float(value) * 60)
+    duration = datetime(1, 1, 1) + seconds
+    days = duration.day - 1
+    print duration
+    if days > 1:
+        return _("%s days") % days
+    if days > 0:
+        return _("%s day") % days
+    if duration.hour > 0:
+        return _("%(hour)sH%(min)s") % {'hour': duration.hour, 'min': "%s" % duration.minute if duration.minute > 0 else ""}
+    return _("%s min.") % duration.minute
 
 
 @register.filter
