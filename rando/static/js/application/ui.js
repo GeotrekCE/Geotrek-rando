a6489760195dac57be939d796862306260f0e7ee
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-08-09 10:29:24 +0200

    Explicit selectors

--- rando/static/js/application/ui.js
+++ rando/static/js/application/ui.js
@@ -130,7 +130,9 @@ function page_load() {
 
 function view_home() {
     sliders();
-    
+
+    // Load filters (will refresh backpack results)
+    // (After sliders initialization)
     window.trekFilter.load();
 
     $('#clear-filters').off('click').on('click', function () {
@@ -246,12 +248,12 @@ function refresh_backpack() {
             trekid = trek.properties.pk;
         if (window.backPack.contains(trekid)) {
             $('#backpack-trek-'+trekid).show(200);
-            $('#trek-' + trekid + ' .btn.backpack').addClass('active').attr('title', gettext('Remove from favorites')).find('i').removeClass('add').addClass('remove');
+            $('#results #trek-' + trekid + ' .btn.backpack').addClass('active').attr('title', gettext('Remove from favorites')).find('i').removeClass('add').addClass('remove');
             $(".detail-content .btn[data-pk='"+ trekid + "']").addClass('active');
         }
         else {
             $('#backpack-trek-'+trekid).hide(200);
-            $('#trek-' + trekid + ' .btn.backpack').removeClass('active').attr('title', gettext('Add to favorites')).find('i').removeClass('remove').addClass('add');
+            $('#results #trek-' + trekid + ' .btn.backpack').removeClass('active').attr('title', gettext('Add to favorites')).find('i').removeClass('remove').addClass('add');
             $(".detail-content .btn[data-pk='"+ trekid + "']").removeClass('active');
         }
     }
