0ffc8514cf6f11106db7c637b2bad9ff5cc50465
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-11-20 17:29:18 +0100

    Show popup only at first visit (ref #31)

--- rando/static/js/popup.js
+++ rando/static/js/popup.js
@@ -1,7 +1,15 @@
 $(document).ready(function (e) {
-    var showModal = true;
 
-    if (showModal && MOBILE === false) {
-        $('#popup-home').modal();
+    if (showModal() && MOBILE === false) {
+        $('#popup-home').modal()
+                        .on('hidden', onPopupClose);
+    }
+
+    function showModal() {
+        return localStorage.getItem('popup-shown') !== "yes";
+    }
+
+    function onPopupClose() {
+        localStorage.setItem('popup-shown', "yes");
     }
 });
