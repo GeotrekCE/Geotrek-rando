d457b4aeed919e91545e112e79685a73ca4fdbfb
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-11-21 15:55:25 +0100

    Lang switcher improvements (ref #31)

--- rando/static/js/popup.js
+++ rando/static/js/popup.js
@@ -2,22 +2,21 @@ $(document).ready(function (e) {
 
     var $modal = $('#popup-home').modal({show: false});
 
-    // Hide popup for links not leaving page
+    // Links not leaving page
     $("#popup-home #popup-body a:not([href^='http'])").click(function () {
         $modal.modal('hide');
         setTimeout(function () {
             window.trekFilter.load();
         }, 0);
     });
-    // Remember that popup was show for others links
+    // External links
     $("#popup-home #popup-body a[href^='http']").click(rememberMe);
-
+    // Popup close
     $modal.on('hidden', rememberMe);
 
     if (showModal() && MOBILE === false) {
         $modal.modal('show');
     }
-
 
     function showModal() {
         var alreadyShown = localStorage.getItem('popup-shown') === "yes",
