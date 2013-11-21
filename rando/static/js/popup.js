202f6ab14c35159975db70bc676c06b55e74f67d
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-11-21 12:47:25 +0100

    Filter treks results on popup links (ref #31)

--- rando/static/js/popup.js
+++ rando/static/js/popup.js
@@ -1,9 +1,23 @@
 $(document).ready(function (e) {
 
+    var $modal = $('#popup-home').modal({show: false});
+
+    // Hide popup for links not leaving page
+    $("#popup-home #popup-body a:not([href^='http'])").click(function () {
+        $modal.modal('hide');
+        setTimeout(function () {
+            window.trekFilter.load();
+        }, 0);
+    });
+    // Remember that popup was show for others links
+    $("#popup-home #popup-body a[href^='http']").click(rememberMe);
+
+    $modal.on('hidden', rememberMe);
+
     if (showModal() && MOBILE === false) {
-        $('#popup-home').modal()
-                        .on('hidden', onPopupClose);
+        $modal.modal('show');
     }
+
 
     function showModal() {
         var alreadyShown = localStorage.getItem('popup-shown') === "yes",
@@ -12,7 +26,7 @@ $(document).ready(function (e) {
         return !alreadyShown && landingHome && noFilter;
     }
 
-    function onPopupClose() {
+    function rememberMe() {
         localStorage.setItem('popup-shown', "yes");
     }
 });
