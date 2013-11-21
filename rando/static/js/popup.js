2f436f031a0072a702d0c5527823b321e2fc5a8b
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-11-21 17:09:25 +0100

    Add ability to show random trek section (ref #31)

--- rando/static/js/popup.js
+++ rando/static/js/popup.js
@@ -14,6 +14,8 @@ $(document).ready(function (e) {
     // Popup close
     $modal.on('hidden', rememberMe);
 
+    enhanceTrekPreviews();
+
     if (showModal() && MOBILE === false) {
         $modal.modal('show');
     }
@@ -27,5 +29,40 @@ $(document).ready(function (e) {
 
     function rememberMe() {
         localStorage.setItem('popup-shown', "yes");
+    }
+
+    function enhanceTrekPreviews () {
+        $('#popup-home [data-trek]').each(function () {
+            var $this = $(this);
+            var trekId = $this.data('trek');
+
+            if (/random/.test(trekId)) {
+                trekId = randomTrek();
+            }
+
+            var trek = getTrek(trekId);
+            if (!trek) return;
+
+            $this.removeAttr('data-trek');
+            $this.find('img.preview').attr('src', trek.properties.pictures[0].url)
+                                     .attr('alt', trek.properties.pictures[0].legend);
+            $this.find('a.profile').attr('href', '/' + trek.properties.slug)
+                                   .addClass('pjax');
+        });
+
+        function randomTrek() {
+            var i = Math.floor(Math.random() * window.treks.features.length),
+                trek = window.treks.features[i];
+            return trek.id;
+        }
+
+        function getTrek(trekid) {
+            for (var i=0, n=window.treks.features.length; i<n; i++) {
+                var trek = window.treks.features[i];
+                if (trek.id === trekid || trek.properties.slug == trekid)
+                    return trek;
+            }
+            return null;
+        }
     }
 });
