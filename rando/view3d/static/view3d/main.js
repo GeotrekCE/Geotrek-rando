22b4f5e0494905adeb3e28444017116214653c9f
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-22 12:59:30 +0100

    Various fixes after merge

--- rando/view3d/static/view3d/main.js
+++ rando/view3d/static/view3d/main.js
@@ -3,7 +3,7 @@ $(window).on("view:detail", function(e) {
     var view3d_url = $("#popup-view3d").data('url');
 
     $("a.view3d").click(function () {
-        var slug = window.trek.properties.slug,
+        var slug = $(this).data('slug'),
             url = view3d_url.replace('empty', slug);
         $('<iframe />', {
             name: 'frame1',
