225903e8eea3429125c9e72b03b4a996d0a891a0
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-23 18:24:30 +0100

    Serious backpack testing

--- rando/trekking/tests/test_popup.js
+++ rando/trekking/tests/test_popup.js
@@ -13,33 +13,33 @@ casper.test.begin('Welcome popup is only shown the first time', function(test) {
 
     casper.start(home_url, function () {
         // On DOM ready.
-        casper.test.assertExists("header a.home.popup",
-                                 'Link to show popup is present');
+        test.assertExists("header a.home.popup",
+                          'Link to show popup is present');
     });
 
     casper.waitUntilVisible('#popup-home', function () {
-        casper.test.comment('Popup is shown by default.');
+        test.pass('Popup is shown by default.');
         casper.click('#popup-home button.close');
     });
 
     casper.waitWhileVisible('#popup-home', function () {
-        casper.test.assertExists("#popup-home[aria-hidden='true']",
-                                 'Popup is now closed.');
+        test.assertExists("#popup-home[aria-hidden='true']",
+                          'Popup is now closed.');
     });
 
     casper.then(function () {
-        casper.test.comment('Come back on home');
+        test.info('Come back on home');
         casper.open(home_url);
     });
 
-    casper.waitUntilVisible('#popup-home', undefined, function () {
-        casper.test.comment('Popup is not shown on second visit.');
+    casper.waitForSelector("#popup-home[aria-hidden='true']", function () {
+        test.pass('Popup is not shown on second visit.');
         casper.click('header a.home.popup');
-    }, 200);
+    });
 
     casper.waitUntilVisible('#popup-home', function () {
-        casper.test.assertExists("#popup-home[aria-hidden='false']",
-                                 'Popup still opens on button click.');
+        test.assertExists("#popup-home[aria-hidden='false']",
+                          'Popup still opens on button click.');
     });
 
     casper.then(utils.clearLocalStorage);
@@ -48,21 +48,19 @@ casper.test.begin('Welcome popup is only shown the first time', function(test) {
      * Landing on other page than home
      */
     casper.then(function () {
-        casper.test.comment('Open detail page');
+        test.info('Open detail page');
         casper.open(home_url + 'pages/flatpage');
     });
 
-    casper.waitUntilVisible('#popup-home', undefined, function () {
-        casper.test.assertExists("#popup-home[aria-hidden='true']",
-                                 'Popup is not shown if landing on other page.');
-
+    casper.waitForSelector("#popup-home[aria-hidden='true']", function () {
+        test.pass('Popup is not shown if landing on other page.');
         casper.click('header a.home');
-    }, 200);
+    });
 
     casper.waitUntilVisible('#popup-home', function () {
-        casper.test.assertExists("#popup-home[aria-hidden='false']",
-                                 'Popup opens on button click.');
-        casper.test.assertUrlMatch(/fr\/#results$/, 'And home page is shown.');
+        test.assertExists("#popup-home[aria-hidden='false']",
+                          'Popup opens on button click.');
+        test.assertUrlMatch(home_url, 'And home page is shown.');
     });
 
     utils.done(test);
