bb748391363398d88817d4a930f16bf6ddeb8490
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-29 17:22:09 +0100

    Prepare release 1.27a

--- rando/wsgi.py
+++ rando/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rando.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rando.settings.prod")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
