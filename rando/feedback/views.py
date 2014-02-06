cc3721cb4ffc5afbd3e2615276cba0fa25406763
Natsu- <romain.garrigues.cs@gmail.com>
2014-02-06 17:19:03 +0100

    Adding email base sending

--- rando/feedback/views.py
+++ rando/feedback/views.py
@@ -2,8 +2,10 @@
 
 import json
 
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
 from .forms import FeedBackForm
 
@@ -11,17 +13,34 @@ from .forms import FeedBackForm
 class FeedBackView(FormView):
 
     template_name = 'feedback/form.html'
+    mail_template_name = 'feedback/mail_template.html'
     form_class = FeedBackForm
+
+    def _send_mail(self, **kwargs):
+
+        subject = _(u'Feedback from {email}').format(email=kwargs['email'])
+        message = render_to_string(
+            self.mail_template_name, kwargs)
+
+        from_mail = 'from@example.com'
+        to_mail = ['to@example.com']
+        send_mail(subject, message, from_mail, to_mail, fail_silently=False)
 
     def form_valid(self, form):
 
         if not self.request.is_ajax():
             return super(FeedBackView, self).form_valid(form)
         else:
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            category = form.cleaned_data['category']
-            comment = form.cleaned_data['comment']
+            category_val = form.cleaned_data['category']
+            data = dict(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                category=dict(form.fields['category'].choices)[category_val],
+                user_comment=form.cleaned_data['comment'],
+            )
+
+            # Send mail to administrator
+            self._send_mail(**data)
 
             response = {'status': 'OK'}
             return HttpResponse(json.dumps(response),
