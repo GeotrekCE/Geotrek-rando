692831d97be92fd054067a3c35517b6b27272a94
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-02-12 09:53:14 +0100

    Add polyfill for Array.prototype.some

--- rando/static/js/ie-polyfill.js
+++ rando/static/js/ie-polyfill.js
@@ -19,6 +19,28 @@
         };
     }
 
+    if (!Array.prototype.some) {
+      Array.prototype.some = function(fun /*, thisArg */) {
+        'use strict';
+
+        if (this === void 0 || this === null)
+          throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== 'function')
+          throw new TypeError();
+
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++) {
+          if (i in t && fun.call(thisArg, t[i], i, t))
+            return true;
+        }
+
+        return false;
+      };
+    }
+
 
     if (win.console === undefined) {
         win.console = {};
