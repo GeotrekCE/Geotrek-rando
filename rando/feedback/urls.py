53a412ab22b5f3981b4cda29170b3d30d1ad86d6
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-13 18:16:52 +0100

    Beginning leaflet trek integration in form

--- rando/feedback/urls.py
+++ rando/feedback/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf8 -*-
 
 from django.conf.urls import patterns, url
-from .views import FeedBackView
+from .views import FeedBackView, LeafletView
 
 
 urlpatterns = patterns(
     '',
-    url(r'feedback/$', FeedBackView.as_view(), name='feedback_view'),
+    url(r'feedback/(?P<slug>[-\w]+)$', FeedBackView.as_view(), name='feedback_view'),
+    #url(r'leaflet/(?P<slug>[-\w]+)/$', LeafletView.as_view(), name='leaflet_view'),
 )
