5cd68febaa3f07e06250d451b80a8e5f2d4cde7d
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-18 10:51:56 +0100

    Categories on feedback form are now language dependant + adding label on each field

--- rando/feedback/forms.py
+++ rando/feedback/forms.py
@@ -3,8 +3,9 @@
 from captcha.fields import ReCaptchaField
 
 from django import forms
+from django.conf import settings
 from django.forms.widgets import Textarea
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language, ugettext_lazy as _
 
 
 class FeedBackForm(forms.Form):
@@ -15,15 +16,26 @@ class FeedBackForm(forms.Form):
         ('SR', 'Senior'),
     )
 
-    name = forms.CharField(max_length=200)
-    email = forms.EmailField(max_length=200)
-    category = forms.ChoiceField(categories)
+    name = forms.CharField(max_length=200, label=_('Name'))
+    email = forms.EmailField(max_length=200, label=_('Email'))
+    category = forms.ChoiceField(categories, label=_('Category'))
 
     comment = forms.CharField(widget=Textarea(
         attrs={'rows': 3, 'cols': 50,
-               'placeholder': _(u'Description of your problem')}))
+               'placeholder': _(u'Description of your problem')}),
+        label=_('Description'))
 
     latitude = forms.FloatField(required=False, label=_(u'Lat'))
     longitude = forms.FloatField(required=False, label=_(u'Lng'))
 
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(label=_(u'Captcha'))
+
+    def __init__(self, *args, **kwargs):
+
+        super(FeedBackForm, self).__init__(*args, **kwargs)
+
+        categories = settings.FEEDBACK_FORM_CATEGORIES.get(
+            get_language(),
+            settings.FEEDBACK_FORM_CATEGORIES.get(settings.LANGUAGE_CODE, ''))
+
+        self.fields['category'].choices = categories
