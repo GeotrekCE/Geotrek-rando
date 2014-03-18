7ce1e9dc0ff0e58d95be026877e9b9631e6447c3
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-18 10:05:45 +0100

    Adding placeholder on comment, which is now mandatory, and adding labels to lat/lng fields

--- rando/feedback/forms.py
+++ rando/feedback/forms.py
@@ -4,6 +4,7 @@ from captcha.fields import ReCaptchaField
 
 from django import forms
 from django.forms.widgets import Textarea
+from django.utils.translation import ugettext_lazy as _
 
 
 class FeedBackForm(forms.Form):
@@ -18,10 +19,11 @@ class FeedBackForm(forms.Form):
     email = forms.EmailField(max_length=200)
     category = forms.ChoiceField(categories)
 
-    comment = forms.CharField(required=False,
-                              widget=Textarea(attrs={'rows': 2}))
+    comment = forms.CharField(widget=Textarea(
+        attrs={'rows': 3, 'cols': 50,
+               'placeholder': _(u'Description of your problem')}))
 
-    latitude = forms.FloatField(required=False)
-    longitude = forms.FloatField(required=False)
+    latitude = forms.FloatField(required=False, label=_(u'Lat'))
+    longitude = forms.FloatField(required=False, label=_(u'Lng'))
 
     captcha = ReCaptchaField()
