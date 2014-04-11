915e9ff6c0ea0ae51d93271a2bfb8c8170462666
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-04-11 10:15:04 +0200

    Toggle fullscreen for 3D view

--- rando/view3d/static/view3d/main.js
+++ rando/view3d/static/view3d/main.js
@@ -5,15 +5,54 @@ $(window).on("view:detail", function(e) {
     $("a.view3d").click(function () {
         var slug = $(this).data('slug'),
             url = view3d_url.replace('empty', slug);
+
+        var $popup = $("#popup-view3d"),
+            $modalBody = $('.modal-body', $popup);
+
         $('<iframe />', {
             name: 'frame1',
             id: 'frame1',
             frameBorder: 0,
             src: url
-        }).appendTo('#popup-view3d .modal-body');
-        $("#popup-view3d").modal('show');
-        $('#popup-view3d').on('hidden', function () {
-            $(this).find('iframe').remove();
+        }).appendTo($modalBody);
+
+        $popup.modal('show');
+        $popup.on('hidden', function () {
+            $popup.find('iframe').remove();
+        });
+
+        $popup.find('a.toggle-fullscreen').click(function () {
+            var $fullscreenBtn = $(this);
+
+            $fullscreenBtn.toggleClass('active');
+            $modalBody.toggleClass('is-fullscreen');
+
+            if ($fullscreenBtn.hasClass('active')) {
+
+                var elem = $modalBody[0];
+                if (elem.requestFullscreen) {
+                  elem.requestFullscreen();
+                } else if (elem.msRequestFullscreen) {
+                  elem.msRequestFullscreen();
+                } else if (elem.mozRequestFullScreen) {
+                  elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullscreen) {
+                  elem.webkitRequestFullscreen();
+                }
+            }
+            else {
+
+                if (document.exitFullscreen) {
+                  document.exitFullscreen();
+                } else if (document.msExitFullscreen) {
+                  document.msExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                  document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                  document.webkitExitFullscreen();
+                }
+            }
+
         });
     });
 });
