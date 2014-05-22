c2930be789d8fa4b8f8d8a3a5c2cca3eb103f74c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-22 17:12:32 +0200

    Rewrite tests to remove email tests

--- rando/feedback/tests/__init__.py
+++ rando/feedback/tests/__init__.py
@@ -1,9 +1,8 @@
+import mock
 import json
 import os
 
 from django.conf import settings
-from django.core import mail
-from django.core.mail.backends.base import BaseEmailBackend
 from django.test import TestCase
 from django.test.utils import override_settings
 
@@ -41,6 +40,18 @@ class FeedBackBaseTests(TestCase):
 
 class FeedBackFormValidationTests(FeedBackBaseTests):
 
+    def setUp(self):
+        super(FeedBackFormValidationTests, self).setUp()
+        self.form_data_ok = {
+            'name': 'John Doe',
+            'email': 'john.doe@nowhere.com',
+            'category': self.first_category[0],
+            'comment': u'This is a comment',
+            # This value 'PASSED' is a django-recaptcha default value
+            # only usable in TEST env to validate form
+            'recaptcha_response_field': 'PASSED',
+        }
+
     def test_form_url(self):
         response = self.client.get(self.feedback_url)
         self.assertContains(response, 'name')
@@ -59,94 +70,32 @@ class FeedBackFormValidationTests(FeedBackBaseTests):
         self.assertEquals(result['status'], 'FORM_INVALID')
         self.assertIn('data', result.keys())
 
-    def test_ajax_post_recaptcha_not_valid(self):
-        form_data_nok = {
-            'name': 'John Doe',
-            'email': 'john.doe@nowhere.com',
-            'category': u'SR',
-        }
+    @mock.patch('rando.feedback.helpers.send_report')
+    def test_ajax_post_recaptcha_not_valid(self, send_report):
+        form_data_nok = dict(**self.form_data_ok)
+        form_data_nok['recaptcha_response_field'] = 'WRONG'
 
         response = self.ajax_post(self.feedback_url, form_data_nok)
 
         result = json.loads(response.content)
         self.assertEquals(result['status'], 'FORM_INVALID')
+        self.assertFalse(send_report.called)
 
-    def test_ajax_post_valid(self):
-        form_data_ok = {
-            'name': 'John Doe',
-            'email': 'john.doe@nowhere.com',
-            'category': self.first_category[0],
-            'comment': u'This is a comment',
-            # This value 'PASSED' is a django-recaptcha default value
-            # only usable in TEST env to validate form
-            'recaptcha_response_field': 'PASSED',
-        }
-
-        response = self.ajax_post(self.feedback_url, form_data_ok)
+    @mock.patch('rando.feedback.helpers.send_report')
+    def test_ajax_post_valid(self, send_report):
+        response = self.ajax_post(self.feedback_url, self.form_data_ok)
 
         result = json.loads(response.content)
-        self.assertEquals(result['status'], 'EMAIL_SENDING_OK')
+        self.assertEquals(result['status'], 'REPORT_OK')
+        self.assertTrue(send_report.called)
 
-
-# This Email Backend is used to test error management when sending email
-class FakeEmailBackend(BaseEmailBackend):
-
-    def send_messages(self, email_messages):
-        raise Exception('Fake problem')
-
-
-class FeedBackEmailSendingTests(FeedBackBaseTests):
-
-    def test_sending_email(self):
-        form_data_ok = {
-            'name': u'John Doe',
-            'email': u'john.doe@nowhere.com',
-            'category': self.first_category[0],
-            'comment': u'This is a comment',
-            'latitude': 1.13,
-            'longitude': 2.26,
-            # This value 'PASSED' is a django-recaptcha default value
-            # only usable in TEST env to validate form
-            'recaptcha_response_field': 'PASSED',
-        }
-
-        # Sending feedback demand, which send a mail if form is OK
-        response = self.ajax_post(self.feedback_url, form_data_ok)
-
+    @mock.patch('rando.feedback.helpers.send_report')
+    def test_ajax_post_valid_send_fails(self, send_report):
+        send_report.side_effect = Exception('Boom!')
+        response = self.ajax_post(self.feedback_url, self.form_data_ok)
         result = json.loads(response.content)
-        self.assertEqual(result['status'], 'EMAIL_SENDING_OK')
+        self.assertEquals(result['status'], 'REPORT_FAILED')
 
-        # Checking that a mail has been sent
-        self.assertEquals(len(mail.outbox), 1)
-        sent_mail = mail.outbox[0]
 
-        self.assertEquals(sent_mail.subject,
-                          u'[Django] Feedback from John Doe')
-
-        settings.FEEDBACK_FORM_CATEGORIES['en'][0][0]
-
-        txt = (u"John Doe (john.doe@nowhere.com) has sent a feedback "
-               "on category ")
-        txt += self.first_category[1]
-        self.assertIn(txt, sent_mail.body)
-        self.assertIn(u"Comment : This is a comment",
-                      sent_mail.body)
-        self.assertIn(u"Lat : 1.13 / Lon : 2.26",
-                      sent_mail.body)
-
-    @override_settings(EMAIL_BACKEND='rando.feedback.tests.FakeEmailBackend')
-    def test_problem_when_sending_email(self):
-        form_data_ok = {
-            'name': 'John Doe',
-            'email': 'john.doe@nowhere.com',
-            'category': self.first_category[0],
-            'comment': u'This is a comment',
-            # This value 'PASSED' is a django-recaptcha default value
-            # only usable in TEST env to validate form
-            'recaptcha_response_field': 'PASSED',
-        }
-
-        response = self.ajax_post(self.feedback_url, form_data_ok)
-
-        result = json.loads(response.content)
-        self.assertEquals(result['status'], 'EMAIL_SENDING_FAILED')
+class SendReportTestCase(TestCase):
+    pass
