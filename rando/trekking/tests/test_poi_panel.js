e50ecef0d6c19b8cef75007f3fc3aa53c68a68ca
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-10-20 17:32:58 +0200

    Do not show POI panel switcher if no POI

--- rando/trekking/tests/test_poi_panel.js
+++ rando/trekking/tests/test_poi_panel.js
@@ -9,14 +9,42 @@ casper.test.begin('Trek detail shows list of POIs', function(test) {
 
     casper.start(detail_url, function () {
         test.assertSelectorHasText('#detail-content-title .title',
-                                   "Découverte de la Cascade d'Ars",
+                                   "Boucle du Pic des Trois Seigneurs",
                                    'Title is shown');
+        // Wait for map ready
+        casper.waitForSelector('.leaflet-control-minimap-toggle-display');
+    });
+
+    casper.then(function () {
+        // Activate map
+        casper.click('#detailmap');
         casper.waitForSelector('.toggle-layer.pois');
     });
 
     casper.then(function () {
         test.assertExists('.toggle-layer.pois.active',
             'POI layer switcher is active.');
+
+        test.info('Trek with no POI');
+    });
+
+
+    var no_poi_detail_url = casper.cli.options['url-base'] + '/fr/decouverte-de-la-cascade-dars';
+
+    casper.thenOpen(no_poi_detail_url, function () {
+        // Wait for map ready
+        casper.waitForSelector('.leaflet-control-minimap-toggle-display');
+    });
+
+    casper.then(function () {
+        // Activate map
+        casper.click('#detailmap');
+        casper.waitForSelector('.toggle-layer.satellite');
+    });
+
+    casper.then(function () {
+        test.assertNotExists('.toggle-layer.pois',
+            'POI layer switcher is not present if no POI.');
     });
 
     utils.done(test);
