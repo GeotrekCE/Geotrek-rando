1f00dab95c338d4caa1de2bd6974e6a5d7e3ccc0
SBats <simon.bats12@gmail.com>
2015-01-14 18:48:20 +0100

    add function to parse img url and add domain name

--- app/modules/treks/services.js
+++ app/modules/treks/services.js
@@ -4,6 +4,32 @@ function treksService(settingsFactory, $resource, $q) {
 
     var self = this;
 
+    this.replaceImgURLs = function (treksData) {        
+
+        // Parse trek pictures, and change their URL
+        angular.forEach(treksData.features, function(trek) {
+            angular.forEach(trek.properties.pictures, function(picture) {
+                picture.url = settingsFactory.DOMAIN + picture.url;
+            });
+            angular.forEach(trek.properties.usages, function(usage) {
+                usage.pictogram = settingsFactory.DOMAIN + usage.pictogram;
+            });
+            angular.forEach(trek.properties.themes, function(theme) {
+                theme.pictogram = settingsFactory.DOMAIN + theme.pictogram;
+            });
+            angular.forEach(trek.properties.networks, function(network) {
+                network.pictogram = settingsFactory.DOMAIN + network.pictogram;
+            });
+            angular.forEach(trek.properties.information_desks, function(information_desk) {
+                information_desk.photo_url = settingsFactory.DOMAIN + information_desk.photo_url;
+            });
+            trek.properties.thumbnail = settingsFactory.DOMAIN + trek.properties.thumbnail;
+            trek.properties.difficulty.pictogram = settingsFactory.DOMAIN + trek.properties.difficulty.pictogram;
+            trek.properties.altimetric_profile = settingsFactory.DOMAIN + trek.properties.altimetric_profile.replace(".json", ".svg");
+        });
+        return treksData;
+    };
+
     this.getTreks = function () {
 
         var deferred = $q.defer();
@@ -17,15 +43,17 @@ function treksService(settingsFactory, $resource, $q) {
 
             var requests = $resource(url, {}, {
                 query: {
-                    method: 'GET'
+                    method: 'GET',
+                    cache: true
                 }
             });
 
             requests.query().$promise
                 .then(function (file) {
                     var data = angular.fromJson(file);
-                    self._trekList = data;
-                    deferred.resolve(data);
+                    var convertedImgs = self.replaceImgURLs(data);
+                    self._trekList = convertedImgs;
+                    deferred.resolve(convertedImgs);
                 });
 
         }
