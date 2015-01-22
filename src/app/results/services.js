fdcf1a0cfc27d90fce5c2df872f7cae15582bd2c
SBats <simon.bats12@gmail.com>
2015-01-22 19:09:23 +0100

    add of results service allowing serving full collection

---
+++ src/app/results/services.js
@@ -0,0 +1,109 @@
+'use strict';
+
+function resultsService($q, globalSettings, treksService, contentsService, eventsService) {
+    this.getAllResults = function () {
+        var deferred = $q.defer(),
+            promises = [],
+            results = [];
+
+        if (globalSettings.ENABLE_TREKS) {
+            promises.push(
+                treksService.getTreks()
+                    .then(
+                        function (treks) {
+                            _.forEach(treks.features, function (trek) {
+                                results.push(trek);
+                            });
+                        }
+                    )
+            );
+        }
+
+        if (globalSettings.ENABLE_TOURISTIC_CONTENT) {
+            promises.push(
+                contentsService.getContents()
+                    .then(
+                        function (contents) {
+                            _.forEach(contents.features, function (content) {
+                                results.push(content);
+                            });
+                        }
+                    )
+            );
+        }
+
+        if (globalSettings.ENABLE_TOURISTIC_EVENTS) {
+            promises.push(
+                eventsService.getEvents()
+                    .then(
+                        function (trEvents) {
+                            _.forEach(trEvents.features, function (trEvent) {
+                                results.push(trEvent);
+                            });
+                        }
+                    )
+
+            );
+        }
+
+        $q.all(promises)
+            .then(
+                function () {
+                    deferred.resolve(results);
+                }
+            );
+
+        return deferred.promise;
+    };
+
+    this.getAResult = function (elementSlug) {
+        var deferred = $q.defer();
+
+        if (globalSettings.ENABLE_TREKS) {
+            treksService.getTreks()
+                .then(
+                    function (treks) {
+                        _.forEach(treks.features, function (trek) {
+                            if (trek.properties.slug === elementSlug) {
+                                deferred.resolve(trek);
+                            }
+                        });
+                    }
+                );
+        }
+
+        if (globalSettings.ENABLE_TOURISTIC_CONTENT) {
+
+            contentsService.getContents()
+                .then(
+                    function (contents) {
+                        _.forEach(contents.features, function (content) {
+                            if (content.properties.slug === elementSlug) {
+                                deferred.resolve(content);
+                            }
+                        });
+                    }
+                );
+        }
+
+        if (globalSettings.ENABLE_TOURISTIC_EVENTS) {
+            eventsService.getEvents()
+                .then(
+                    function (trEvents) {
+                        _.forEach(trEvents.features, function (trEvent) {
+                            if (trEvent.properties.slug === elementSlug) {
+                                deferred.resolve(trEvent);
+                            }
+                        });
+                    }
+                );
+
+        }
+
+        return deferred.promise;
+    };
+}
+
+module.exports = {
+    resultsService: resultsService
+};
