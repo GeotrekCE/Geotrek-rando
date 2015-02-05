ca47fb27d5e694bf2e40318c5e9c4c6165820953
SBats <simon.bats12@gmail.com>
2015-02-05 16:05:45 +0100

    add maps folders in repo

--- gulp/tasks/vendors.js
+++ gulp/tasks/vendors.js
@@ -50,7 +50,8 @@ gulp.task('vendors', function () {
         .bundle()
         .on('error', handleErrors)
         .pipe(exorcist(
-            config.dest + '/maps/' + config.outputName + '.map'
+            config.dest + '/maps/' + config.outputName + '.map',
+            'maps/' + config.outputName + '.map'
         ))
         .pipe(source(config.outputName))
         .pipe(streamify(uglify()))
