0bec15e1bdfffa8102883d2d48d9ff4705ec6fae
SBats <simon.bats12@gmail.com>
2015-02-25 15:48:13 +0100

    better display for pois liste

--- src/app/pois/controllers.js
+++ src/app/pois/controllers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function PoisListeController($scope) {
+
     $scope.hoverMarkerPoi = function (currentPoi, state) {
         var layerEquivalent = document.querySelector('.layer-' + currentPoi.properties.type.id + '-' + currentPoi.id);
         if (layerEquivalent !== null) {
@@ -15,6 +16,31 @@ function PoisListeController($scope) {
             }
         }
     };
+
+    function initListeClasses() {
+        $scope.isOpened = {};
+        _.each($scope.pois, function (poi) {
+            $scope.isOpened[poi.id] = 'closed';
+        });
+    }
+
+    $scope.togglePoi = function (poiId) {
+        if ($scope.isOpened[poiId] === 'closed') {
+            _.forEach($scope.isOpened, function (isOpenedValue, index) {
+                if (parseInt(index, 10) === parseInt(poiId, 10)) {
+                    $scope.isOpened[index] = 'opened';
+                } else {
+                    $scope.isOpened[index] = 'hidden';
+                }
+            });
+        } else {
+            _.forEach($scope.isOpened, function (isOpenedValue, index) {
+                $scope.isOpened[index] = 'closed';
+            });
+        }
+    };
+
+    initListeClasses();
 }
 
 module.exports = {
