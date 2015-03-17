0c71b1754ac1b0d555b93c17f2fa9934eb43f49a
SBats <simon.bats12@gmail.com>
2015-03-17 19:21:59 +0100

    fields and basic graphism for home page

--- src/app/home/controllers.js
+++ src/app/home/controllers.js
@@ -1,7 +1,44 @@
 'use strict';
 
-function HomeController($scope) {
-      
+function HomeController($scope, $location, categoriesService, filtersService, resultsService) {
+
+    function initForm() {
+        $scope.prefilter = {
+            categories: "0",
+            themes: "0"
+        };
+        resultsService.getAllResults()
+            .then(
+                function (data) {
+                    $scope.filters = filtersService.initGlobalFilters(data);
+                    console.log($scope.filters);
+                }
+            );
+        categoriesService.getCategories()
+            .then(
+                function (data) {
+                    $scope.categories = data;
+                    console.log(data);
+                }
+            );
+    }
+
+    $scope.prefilterHome = function () {
+        console.log($scope.prefilter);
+        var query = {};
+
+        if ($scope.prefilter.categories !== "0") {
+            query.categories = $scope.prefilter.categories;
+        }
+
+        if ($scope.prefilter.themes !== "0") {
+            query.themes = $scope.prefilter.themes;
+        }
+
+        $location.search(query);
+    };
+
+    initForm();
 }
 
 module.exports = {
