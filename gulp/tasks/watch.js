f63ffa9d52d02f6ef7445698a8033fcba9249f95
SBats <simon.bats12@gmail.com>
2015-04-15 19:19:09 +0200

    remove useless require

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,6 @@
 
 var gulp     = require('gulp');
 var config   = require('../config');
-var watchify = require('./browserify');
 
 gulp.task('watch', ['watchify', 'browserSync'], function () {
     gulp.watch(config.sass.toWatch, ['sass']);
