9a2f87efd75298fb8a9564908ac881722882924e
Sbats <simon.bats12@gmail.com>
2015-04-27 16:08:55 +0200

    fixed trek dependent conditions

--- src/app/3d/controllers.js
+++ src/app/3d/controllers.js
@@ -9,7 +9,7 @@ function Rando3DController(result, $scope, $timeout, $modalInstance, globalSetti
             IMAGES_FOLDER: 'images/3d/',
             DEM_URL: settingsFactory.trekUrl.replace(/\$lang/, translationService.getCurrentLang().code) + pk + '/' + globalSettings.DEM_FILE,
             PROFILE_URL: settingsFactory.trekUrl.replace(/\$lang/, translationService.getCurrentLang().code) + pk + '/' + globalSettings.PROFILE_FILE,
-            POI_URL: settingsFactory.poisUrl + '?format=geojson&trek=' + pk,
+            POI_URL: settingsFactory.trekUrl.replace(/\$lang/, translationService.getCurrentLang().code) + '/' + pk + '/' + globalSettings.POI_FILE,
             TILE_TEX_URL: "https://a.tiles.mapbox.com/v3/makina-corpus.i3p1001l/{z}/{x}/{y}.png",
             SIDE_TEX_URL: "/images/3d/side.jpg",
             CAM_SPEED_F: 100,
