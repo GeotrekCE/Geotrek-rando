ec585cf107800cd28ed2b6c61038ac8ee78f46a5
Sbats <simon.bats12@gmail.com>
2015-04-29 17:39:35 +0200

    bug fix for cluster hovered class on result hover

--- src/app/results/controllers.js
+++ src/app/results/controllers.js
@@ -28,6 +28,7 @@ function ResultsListeController($scope, $rootScope, globalSettings, utilsFactory
 
     $scope.hoverLayerElement = function (currentElement, state) {
         var layerEquivalent = document.querySelectorAll('.layer-category-' + currentElement.properties.category.id + '-' + currentElement.id);
+
         if (layerEquivalent.length === 0) {
             var mapLayers = [];
             $rootScope.map.eachLayer(function (currentLayer) {
@@ -38,23 +39,22 @@ function ResultsListeController($scope, $rootScope, globalSettings, utilsFactory
             var position = utilsFactory.getStartPoint(currentElement);
             var cluster = L.GeometryUtil.closestLayer($rootScope.map, mapLayers, position);
             if (cluster) {
-                layerEquivalent = cluster.layer._icon;
+                layerEquivalent = [cluster.layer._icon];
             }
-        } else {
-            _.each(layerEquivalent, function (selectedLayer) {
-                if (selectedLayer) {
-                    if (state === 'enter') {
-                        if (!selectedLayer.classList.contains('hovered')) {
-                            selectedLayer.classList.add('hovered');
-                        }
-                    } else {
-                        if (selectedLayer.classList.contains('hovered')) {
-                            selectedLayer.classList.remove('hovered');
-                        }
+        }
+        _.each(layerEquivalent, function (selectedLayer) {
+            if (selectedLayer) {
+                if (state === 'enter') {
+                    if (!selectedLayer.classList.contains('hovered')) {
+                        selectedLayer.classList.add('hovered');
+                    }
+                } else {
+                    if (selectedLayer.classList.contains('hovered')) {
+                        selectedLayer.classList.remove('hovered');
                     }
                 }
-            });
-        }
+            }
+        });
     };
 
     $scope.favIcon = (globalSettings.FAVORITES_ICON ? globalSettings.FAVORITES_ICON : 'heart');
