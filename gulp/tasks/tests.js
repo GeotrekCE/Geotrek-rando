733c734f36a61bbdac4b6cd8dd4ade66a21f1861
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-05-12 17:34:40 +0200

    Use connect (& portfinder) to serve OTF for tests

--- gulp/tasks/tests.js
+++ gulp/tasks/tests.js
@@ -12,13 +12,33 @@ gulp.task('webdriver_update', webdriver_update);
 var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
 gulp.task('webdriver_standalone', webdriver_standalone);
 
+
+var portfinder = require('portfinder');
+var connect    = require('gulp-connect');
+
+
 // Setting up the test task
 gulp.task('tests:e2e', ['webdriver_update'], function(cb) {
-    gulp.src(['src/tests/e2e/**/*.specs.js']).pipe(protractor({
-        configFile: 'protractor.conf.js',
-    })).on('error', function(e) {
-        console.log(e)
-    }).on('end', cb);
+
+    portfinder.getPort(function (err, port) {
+        if (err) return;
+
+        connect.server({
+            root: 'src',
+            port: port,
+        });
+
+        gulp.src(['src/tests/e2e/**/*.specs.js']).pipe(protractor({
+            configFile: 'protractor.conf.js',
+            args: [
+                '--baseUrl', 'http://localhost:' + port
+            ]
+        })).on('error', function(e) {
+            connect.serverClose();
+            console.log(e)
+        }).on('end', connect.serverClose)
+        .on('end', cb);
+    });
 });
 
 gulp.task('tests', [
