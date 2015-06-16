21af84b00001b8b7cd40881b072a9cefb2a2235d
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-06-16 09:07:08 +0200

    Init UI language before each UI test
    	TODO: find a way to do it once for each spec (and remove timeconsuming .get('/'))

--- src/tests/e2e/contents.specs.js
+++ src/tests/e2e/contents.specs.js
@@ -1,13 +1,26 @@
-describe('Trecks test data', function() {
+describe('Trecks test data (detailed)', function() {
+
+    beforeEach(function() {
+        browser.get('/');
+        browser.executeScript("localStorage.setItem('geotrek-rando-language', '{\"code\":\"fr\",\"label\":\"English\"}');");
+        browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
+    });
 
     it('should have the right title', function() {
-        browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
         expect(element(by.css('.element-title h1')).getText()).toEqual('Boucle du Pic des Trois Seigneurs');
     });
 
     it('title should have an icon', function() {
         browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
         expect(element(by.css('.element-title .cat-icon svg')).isPresent()).toBe(true);
+    });
+});
+
+describe('Trecks test data (search)', function() {
+
+    beforeEach(function() {
+        browser.get('/');
+        browser.executeScript("localStorage.setItem('geotrek-rando-language', '{\"code\":\"fr\",\"label\":\"English\"}');");
     });
 
     it('should display 8 results when all categories are checked', function () {
