3ca86ce0e6ded604e06f83b4559fd73b862550bc
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-06-18 14:34:30 +0200

    Use jasmine2 as test framework for Protractor (e2e tests)

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,16 +1,17 @@
 var ScreenshotReporter = require('./src/tests/lib/screenshot-reporter.js');
 
 exports.config = {
+    framework: 'jasmine2',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    },
+
     multiCapabilities: [
         {
             browserName: 'firefox',
         }
     ],
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000,
-        isVerbose: true
-    },
 
     onPrepare: function() {
         browser.driver.manage().window().setSize(1280, 1024);
