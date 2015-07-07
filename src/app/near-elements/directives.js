f27abcfdbb110281ea2152fd27784d9dede53aee
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-07-07 17:35:11 +0200

    Cleanups local vars for module: near-element

--- src/app/near-elements/directives.js
+++ src/app/near-elements/directives.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var controllers = require('./controllers');
-
 function nearListeDirective() {
     return {
         restrict: 'E',
         replace: true,
         //scope: false,
         template: require('./templates/near-elements.html'),
-        controller: controllers.NearListeController
+        controller: 'NearListeController'
     };
 }
 
