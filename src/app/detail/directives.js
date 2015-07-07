50c90f03d9641f2af7085f1afee38c9c7ff98ae6
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-07-07 14:41:01 +0200

    Cleanups local vars for module: detail

--- src/app/detail/directives.js
+++ src/app/detail/directives.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var controllers = require('./controllers');
-
 function detailDirective() {
     return {
         restrict: 'E',
         replace: true,
         template: require('./templates/detail-page.html'),
-        controller: controllers.DetailController
+        controller: 'DetailController'
     };
 }
 
--- src/app/detail/index.js
+++ src/app/detail/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var angular = require('angular');
-
 angular.module('rando.detail', ['ui.bootstrap'])
     .controller('DetailController', require('./controllers').DetailController)
     .directive('detailPage', require('./directives').detailDirective);
