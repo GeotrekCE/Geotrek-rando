d85d9398d56c4d044b64ddb68dc7ee26eb7b4cf3
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-07-07 16:07:01 +0200

    Cleanups local vars for module: home

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var angular = require('angular');
-
 angular.module('rando.home', [])
     .controller('HomeController', require('./controllers').HomeController)
     .service('homeService', require('./services').homeService)
