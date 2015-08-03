ff8bf8763b32e17f3ed63437e9334fec9f6d42b6
Sbats <simon.bats12@gmail.com>
2015-08-03 21:17:17 +0200

    update app according to new translation system

--- src/app/layout/index.js
+++ src/app/layout/index.js
@@ -8,7 +8,6 @@ angular.module('rando.layout', ['ui.router', 'rando.treks', 'angular-google-anal
     .controller('SidebarFlatController',   controllers.SidebarFlatController)
     .controller('SidebarDetailController', controllers.SidebarDetailController)
     .config(require('./routes').layoutRoutes)
-    .config(require('./lang').translateLayout)
     .config(require('./analytics').AnalyticsConfig)
     .run(['Analytics', function (Analytics) {}]) // !!! Analytics needs to be injected once in order to be active !!!
     .run(require('./services').RunApp);
--- src/app/translation/configs.js
+++ src/app/translation/configs.js
@@ -1,6 +1,14 @@
 'use strict';
 
 function translationConfig($translateProvider, globalSettings) {
+    $translateProvider.translations('fr', require('../translation/lang/fr.json'));
+
+    $translateProvider.translations('en', require('../translation/lang/en.json'));
+
+    $translateProvider.translations('de', require('../translation/lang/de.json'));
+
+    $translateProvider.translations('nl', require('../translation/lang/nl.json'));
+
     $translateProvider.preferredLanguage(globalSettings.DEFAULT_LANGUAGE);
     $translateProvider.useSanitizeValueStrategy('escape');
 }
