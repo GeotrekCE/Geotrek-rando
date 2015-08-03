c8f0f70973e6d939750f0a2fafb11fe395d0987c
Sbats <simon.bats12@gmail.com>
2015-08-03 12:25:28 +0200

    better e2e tests for warning panel

--- src/app/warning/tests/e2e/warning-panel.spec.js
+++ src/app/warning/tests/e2e/warning-panel.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Test warning panel - ', function() {
-    var panel, button;
+    var panel, toggleButton, form, name, email, category, comment, location, submit;
     beforeAll(function () {
         browser.get('/');
         browser.executeScript(function () {
@@ -12,12 +12,19 @@ describe('Test warning panel - ', function() {
         });
         browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
 
-        button = element(by.css('.detail-actions .signal a'));
-        panel = element(by.css('.warning-panel'));
+        toggleButton = element(by.css('.detail-actions .signal a'));
+        panel        = element(by.css('.warning-panel'));
+        form         = element(by.css('.warning-panel form'));
+        name         = element(by.css('.warning-panel form #warning-name'));
+        email        = element(by.css('.warning-panel form #warning-email'));
+        category     = element(by.css('.warning-panel form #warning-category'));
+        comment      = element(by.css('.warning-panel form #warning-comment'));
+        location     = element(by.css('.warning-panel form #warning-location'));
+        submit       = element(by.css('.warning-panel form input[type="submit"]'));
     });
 
     it('should have the warning button on sidebar', function() {
-        expect(button.isPresent()).toBe(true);
+        expect(toggleButton.isPresent()).toBe(true);
     });
 
     it('should have the warning pannel hidden', function() {
@@ -26,8 +33,56 @@ describe('Test warning panel - ', function() {
     });
 
     it('should display panel on click', function() {
-
-        button.click();
+        toggleButton.click();
         expect(panel.getAttribute('class')).toMatch('opened');
     });
+
+    it('should not validate if no fields filled', function() {
+        expect(form.getAttribute('class')).toMatch('ng-invalid');
+    });
+
+    it('should validate name field if not empty', function() {
+        expect(name.getAttribute('class')).toMatch('untouched');
+        expect(name.getAttribute('class')).toMatch('ng-invalid');
+        name.sendKeys('test');
+        expect(name.getAttribute('class')).toMatch('ng-valid');
+    });
+
+    it('should still not validate form', function() {
+        expect(form.getAttribute('class')).toMatch('ng-invalid');
+    });
+
+    it('should not validate email field if invalid email patern', function() {
+        expect(email.getAttribute('class')).toMatch('untouched');
+        expect(email.getAttribute('class')).toMatch('ng-invalid');
+        email.sendKeys('test');
+        expect(email.getAttribute('class')).toMatch('ng-invalid');
+    });
+
+    it('should validate email field if valid email patern', function() {
+        email.sendKeys('test@test.fr');
+        expect(email.getAttribute('class')).toMatch('ng-valid');
+    });
+
+    it('should already have a value for category field', function() {
+        expect(category.getAttribute('class')).toMatch('untouched');
+        expect(category.getAttribute('class')).toMatch('ng-valid');
+    });
+
+    it('should validate comment field if not empty', function() {
+        expect(comment.getAttribute('class')).toMatch('untouched');
+        expect(comment.getAttribute('class')).toMatch('ng-invalid');
+        comment.sendKeys('test comment');
+        expect(comment.getAttribute('class')).toMatch('ng-valid');
+    });
+
+    it('should already have a value for location field', function() {
+        expect(location.getAttribute('class')).toMatch('untouched');
+        expect(location.getAttribute('class')).toMatch('ng-valid');
+    });
+
+    it('should now valid the form', function() {
+        expect(form.getAttribute('class')).not.toMatch('ng-invalid');
+    });
+
 });
