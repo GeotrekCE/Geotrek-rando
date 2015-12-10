dc2c6d44f55d781707f3ce0434616c18b412c7d2
Alexandra Janin <alexandra.janin@makina-corpus.com>
2015-12-10 11:26:53 +0100

    Add a directive to prevent navigating to #

---
+++ src/app/commons/directives.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function a() {
+    // This function prevent the browser from navigating to # 
+    return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+            if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
+                elem.on('click', function(e){
+                    e.preventDefault();
+                });
+            }
+        }
+   };
+}
+
+module.exports = {
+    a: a
+};
--- src/app/commons/index.js
+++ src/app/commons/index.js
@@ -1,4 +1,5 @@
 'use strict';
 
 angular.module('rando.commons', [])
-    .factory('utilsFactory', require('./factories').utilsFactory);
+.factory('utilsFactory', require('./factories').utilsFactory)
+.directive('a', require('./directives').a);
