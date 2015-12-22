f416b75f948400fa3d4c475adb71eac9552003ea
Alexandra Janin <alexandra.janin@makina-corpus.com>
2015-12-22 13:44:31 +0100

    Add arrows on treks layer

--- src/app/app.js
+++ src/app/app.js
@@ -62,6 +62,7 @@ require('bootstrap');
 require('sparklines');
 
 //MAP LIBRARIES
+require('leaflet-textpath');
 require('leaflet-minimap');
 require('leaflet.markercluster');
 require('leaflet.fullscreen');
