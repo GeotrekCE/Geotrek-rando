3fdd3bbba3c4d422b525c92bf1bf08e8d61ed763
Alexandra Janin <alexandra.janin@makina-corpus.com>
2015-12-23 10:48:55 +0100

    Fix add arrow

--- src/app/map/services.js
+++ src/app/map/services.js
@@ -1073,12 +1073,12 @@ function mapService($q, $state, $resource, $filter, utilsFactory, globalSettings
             self.createLayerFromElement(result, type, elementLocation)
                 .then(
                     function (layer) {
-                        if (globalSettings.SHOW_ARROWS_ON_ROUTE) {
-                            layer.setText('    ›    ', {offset: 12, repeat: true, center: true, attributes: {class: 'arrow-direction category-' + result.properties.category.id}});
-                        }
                         currentLayer.addLayer(layer);
                         self._clustersLayer.addLayer(currentLayer);
                         if (result.geometry.type !== "Point" && result.geometry.type !== "MultiPoint") {
+                            if (globalSettings.SHOW_ARROWS_ON_ROUTE) {
+                                layer.setText('    ›    ', {offset: 12, repeat: true, center: true, attributes: {class: 'arrow-direction category-' + result.properties.category.id}});
+                            }
                             if (globalSettings.ALWAYS_HIGHLIGHT_TREKS) {
                                 self.highlightPath(result, true, true);
                             }
