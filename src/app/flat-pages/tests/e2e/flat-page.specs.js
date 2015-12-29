49f4310df86def3efc5674104f08ff1622d76c52
Sbats <simon.bats12@gmail.com>
2015-12-29 17:02:25 +0100

    add basic test for flat pages

---
+++ src/app/flat-pages/tests/e2e/flat-page.specs.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Geotrek flat page - ', function() {
+    var pageContent;
+
+    beforeAll(function() {
+        browser.get('/');
+        browser.executeScript(function () {
+            localStorage.clear();
+            localStorage.setItem('geotrek-rando-language', JSON.stringify({
+                code: 'fr'
+            }));
+        });
+    });
+
+    it('should display a Flat page', function () {
+        browser.get('/#/informations/test/');
+        pageContent = element(by.css('.flat-page h1'));
+        expect(pageContent.getText()).not.toEqual('');
+    });
+
+});
