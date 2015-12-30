4d08ef30ecf378ed2d5d842adb32aa7c40eafe25
Sbats <simon.bats12@gmail.com>
2015-12-30 16:19:42 +0100

    add new tests for homepage

--- src/app/home/tests/e2e/home.specs.js
+++ src/app/home/tests/e2e/home.specs.js
@@ -2,15 +2,20 @@
 
 describe('Home page ', function() {
     var constants = require('../../../config/settings.constant.json');
+    var homePage;
 
     beforeAll(function() {
         browser.get('/');
+
+        homePage = element(by.id('home-page'));
+    });
+
+    beforeEach(function () {
+        element(by.css('.header-title-link')).click();
     });
 
     it('should display if SHOW_HOME is true in config', function () {
-        expect(
-            element(by.id('home-page')).isPresent()
-        ).toBe(constants.SHOW_HOME);
+        expect(homePage.isPresent()).toBe(constants.SHOW_HOME);
     });
 
 
@@ -19,5 +24,45 @@ describe('Home page ', function() {
 
         expect(numberOfRandomElements).toBeGreaterThan(0);
     });
+
+    it('should have categories prefilters', function () {
+        var categoriesPrefilter = element.all(by.css('.default-categories-prefilter-item'));
+
+        expect(categoriesPrefilter.count()).toBeGreaterThan(0);
+    });
+
+    it('should work for a single category', function () {
+        var categoriesPrefilter = element.all(by.css('.default-categories-prefilter-item'));
+        var category = categoriesPrefilter.first().getAttribute('data-categories');
+        var browserUrl;
+
+        categoriesPrefilter.first().click();
+
+        browserUrl = browser.getCurrentUrl();
+        expect(browserUrl).toMatch(category);
+    });
+
+    it('should work for multiple categories', function () {
+        var categoriesPrefilter = element.all(by.css('.default-categories-prefilter-item'));
+        var categories = categoriesPrefilter.last().getAttribute('data-categories');
+        var browserUrl;
+
+        categoriesPrefilter.last().click();
+
+        browserUrl = browser.getCurrentUrl();
+
+        for (var i = categories.length - 1; i >= 0; i--) {
+            expect(browserUrl).toMatch(categories[i]);
+        };
+    });
+
+    it('should hide if we click on access button', function () {
+        var accessButton = element(by.css('#home-page .hide-home'));
+
+        accessButton.click();
+
+        expect(homePage.isPresent()).toBe(false);
+    });
+
 
 });
