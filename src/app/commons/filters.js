d2584759614cc153a58a1c6de74888713a913eb6
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-01-14 12:40:34 +0100

    Transform sanitizeData service into an angular filter

--- src/app/commons/filters.js
+++ src/app/commons/filters.js
@@ -21,7 +21,17 @@ function isSVG($filter) {
     };
 }
 
+function sanitizeData($filter, $sce) {
+    return function(data, removeStyle) {
+        if (removeStyle) {
+            var data = data.replace(/[a-zA-Z0-9\-\_]*style="[^\"]*"/gim, '');
+        }
+        return $sce.trustAsHtml(data);
+    }
+}
+
 module.exports = {
     removeTags: removeTags,
-    isSVG: isSVG
+    isSVG: isSVG,
+    sanitizeData: sanitizeData
 };
--- src/app/commons/index.js
+++ src/app/commons/index.js
@@ -4,4 +4,5 @@ angular.module('rando.commons', [])
 .factory('utilsFactory', require('./factories').utilsFactory)
 .filter('removeTags', require('./filters').removeTags)
 .filter('isSVG', require('./filters').isSVG)
+.filter('sanitizeData', require('./filters').sanitizeData)
 .directive('a', require('./directives').a);
