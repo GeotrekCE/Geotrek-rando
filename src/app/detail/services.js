51678194499e0438a7011a5408d6eeed578c933e
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-01-14 14:05:22 +0100

    Add condition if informations field are empty

--- src/app/detail/services.js
+++ src/app/detail/services.js
@@ -8,7 +8,7 @@ function detailService() {
     // Retrn true if object contains at least one element of arguments
         var infos = 0;
         for (var i = 0; i < arguments.length; i++) {
-            if (obj[arguments[i]] !== undefined) {
+            if (obj[arguments[i]] !== undefined && obj[arguments[i]] !== '') {
                 infos++;
             }
         }
