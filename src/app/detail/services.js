664e06d30bfd755396c86efef4997599a89da0b6
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-01-14 14:19:41 +0100

    remove useless code

--- src/app/detail/services.js
+++ src/app/detail/services.js
@@ -2,9 +2,7 @@
 
 function detailService() {
 
-    var self = this;
-
-    this.hasInfos = function (obj, args) {
+    this.hasInfos = function (obj) {
     // Retrn true if object contains at least one element of arguments
         var infos = 0;
         for (var i = 0; i < arguments.length; i++) {
