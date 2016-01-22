332b6499848f00d36d9f73e99e7c84dfa8dc1047
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-01-22 17:50:22 +0100

    Add tests for map detail page

---
+++ src/app/map/tests/e2e/map-detail.specs.js
@@ -0,0 +1,37 @@
+'use strict'
+
+describe('map  ', function() {
+    var constants = require('../../../config/settings.constant.json');
+    var trekPath = element(by.css('.leaflet-map-pane .leaflet-overlay-pane svg.leaflet-zoom-animated g'));
+
+    beforeAll(function() {
+        browser.get('/');
+        browser.executeScript(function (constants) {
+            localStorage.clear();
+            localStorage.setItem(constants.PLATFORM_ID + '-language', 'fr');
+        }, constants);
+    });
+
+    afterAll(function () {
+        browser.executeScript(function () {
+            localStorage.clear();
+        });
+    });
+
+    it('should have a visible path on trek detail page', function () {
+        browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
+
+        expect(trekPath.isPresent()).toBe(true);
+    });
+
+    it('should have a visible path on trek detail page after switch language', function () {
+        browser.get('/#/itineraire/boucle-du-pic-des-trois-seigneurs/');
+
+        element(by.css('.languages .dropdown-toggle')).click();
+        element(by.css('.languages .en a')).click();
+        browser.driver.sleep(1);
+        browser.waitForAngular();
+
+        expect(trekPath.isPresent()).toBe(true);
+    });
+});
