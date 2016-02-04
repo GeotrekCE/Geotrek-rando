a24015d71e4ffc6e021fcc2ed14ce6ae37519e22
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-02-04 17:38:10 +0100

    Add condition to `simpleEach` to avoid exceptions

--- src/app/vendors/scripts/simple-each.js
+++ src/app/vendors/scripts/simple-each.js
@@ -1,6 +1,8 @@
 module.exports = function simpleEach (arr, fn) {
     var i = 0;
+    if (!arr) return false;
     var arrLen = arr.length;
+    if (!arrLen) return false;
 
     for (; i < arrLen; i++) {
         fn(arr[i]);
