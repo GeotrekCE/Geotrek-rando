558f34af4b3c1ac78694929b4d766070ab74af08
Sbats <simon.bats12@gmail.com>
2016-02-24 18:05:02 +0100

    contextualise parent for detail view

--- src/app/detail/services.js
+++ src/app/detail/services.js
@@ -2,6 +2,31 @@
 
 function detailService() {
 
+    this.setCurrentParent = function setCurrentParent(parentId) {
+        this.curentParrent = parentId;
+    };
+
+    this.getCurrentParent = function getCurrentParent() {
+        if (!this.curentParrent) {
+            this.curentParrent = null;
+        }
+        return this.curentParrent;
+    };
+
+    this.getStepNumberInParent = function getStepNumberInParent(childId, parent) {
+        if (!parent.properties.children) {
+            return false;
+        }
+        for (var i = 0; i < parent.properties.children.length; i++) {
+            var child = parent.properties.children[i];
+            if (child === childId) {
+                return i+1;
+            }
+        }
+
+        return false;
+    };
+
     this.hasInfos = function hasInfos (obj) {
     // Retrn true if object contains at least one element of arguments
         var infos = 0;
