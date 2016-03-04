18e1c7db78c54fbd69006cde34cf378d1baac8de
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-03-04 15:21:02 +0100

    Use real `settings.custom` from `custom/` instead of symlink

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -3,7 +3,7 @@
 var _ = require('lodash');
 
 angular.module('rando.config', [])
-    .constant('globalSettings', _.assign(require('./settings.default.json'), require('./settings.custom.json')))
+    .constant('globalSettings', _.assign(require('./settings.default.json'), require('../../../custom/settings.custom.json')))
     .config(require('./providers.config').providersConfig)
     .factory('settingsFactory', require('./factories').settingsFactory)
     .service('stylesConfigService', require('./services').stylesConfigService)
