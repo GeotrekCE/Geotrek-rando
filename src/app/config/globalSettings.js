0f358c374b71e33e3ddb06baa06e41d0db53d0dc
PAC <pac@makina-corpus.com>
2018-06-11 14:31:05 +0200

    Document changes around the default active category setting.

--- src/app/config/globalSettings.js
+++ src/app/config/globalSettings.js
@@ -5,11 +5,13 @@ var globalSettings = _.assign(require('./settings.default.json'), require('../..
 // Enforce 'ENABLE_UNIQUE_CAT' to true as the new user-interface cannot have several active categories at the same time.
 globalSettings.ENABLE_UNIQUE_CAT = true;
 
-// Because the new user-interface cannot have several active categories at the same time, we
-// removed the 'DEFAULT_ACTIVE_CATEGORIES' setting. Technically however, the system keeps
-// working the same way and does expect an array of active categories.
-// Hence, set the legacy setting to a single value array stating what category should be
-// active by default.
+/**
+ *  Because the new user-interface cannot have several active categories at the same time, we
+ *  removed the 'DEFAULT_ACTIVE_CATEGORIES' setting. Technically however, the system keeps
+ *  working the same way and does expect an array of active categories.
+ *  Hence, set the legacy setting to a single value array stating what category should be
+ *  active by default.
+ */
 globalSettings.DEFAULT_ACTIVE_CATEGORIES = [globalSettings.DEFAULT_ACTIVE_CATEGORY];
 
 module.exports = globalSettings;
