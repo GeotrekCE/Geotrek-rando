2e655024546da029e66211b575d7dda2dfdcd0ec
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2019-05-16 15:41:37 +0200

    Change setting name

--- src/app/detail/controllers.js
+++ src/app/detail/controllers.js
@@ -6,7 +6,7 @@ function DetailController($scope, $rootScope, $state, $q, $modal, $timeout, $sta
     $scope.removeDiacritics = utilsFactory.removeDiacritics;
     $scope.nearElementsByCategories = globalSettings.NEAR_ELEMENTS_CATEGORIES;
     $scope.displayAsidesCounter = globalSettings.DISPLAY_ASIDES_COUNTERS;
-    $scope.displayCredits = globalSettings.DISPLAY_CREDITS_IMAGE;
+    $scope.displayCredits = globalSettings.DISPLAY_CREDITS_ON_IMAGE;
 
     if (globalSettings.RULES_FLAT_PAGES_ID) {
         $scope.rulesId = globalSettings.RULES_FLAT_PAGES_ID;
